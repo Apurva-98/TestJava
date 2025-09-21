a87ebce73c0ce4540d2f62f98a12a3d2a5766343
Apurva <apurvavyas98@gmail.com>
2025-09-21 16:18:23 +0530

    testfirst

---
+++ src/table9ByPrintf.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class table9ByPrintf {
+    public static void main(String[] args) {
+        int table = 9;
+       System.out.printf("%d*1=%d",table,table*1); //9
+        System.out.printf("%d*1=%d",table,table*2); //9
+        System.out.printf("%d*1=%d",table,table*3); //9
+        System.out.printf("%d*1=%d",table,table*4); //9
+        System.out.printf("%d*1=%d",table,table*5); //9
+        System.out.printf("%d*1=%d",table,table*6); //9
+        System.out.printf("%d*1=%d",table,table*7); //9
+        System.out.printf("%d*1=%d",table,table*8); //9
+        System.out.printf("%d*1=%d",table,table*9); //9
+        System.out.printf("%d*1=%d",table,table*10); //
+
+
+
+    }
+}
